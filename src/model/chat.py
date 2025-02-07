f213da6ec7f1f4a815d2fa9e967a1c649d6aac7b
shun31y <odango1329@gmail.com>
2025-02-08 08:42:28 +0900

    Add chat function to handle user messages and integrate OpenAI API

--- src/model/chat.py
+++ src/model/chat.py
@@ -1,15 +1,23 @@
 import os
+
+import dotenv
 import openai
+
+dotenv.load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 
 def chat_template(user_message: str) -> list[dict[str, str]]:
     messages = [
-        {
-            "role": "system",
-            "content": "You are a helpful assistant."
-        }, 
-        {
-            "role": "user",
-            "content": user_message
-        }
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": user_message},
     ]
     return messages
+
+
+def chat(user_message: str) -> str:
+    client = openai.OpenAI()
+    messages = chat_template(user_message)
+    client.api_key = OPENAI_API_KEY
+    response = client.chat.completions.create(model="gpt-4o", messages=messages)
+    return response.choices[0].message.content
