f07d806b217470502400ecf3c699943415fe89ff
shun31y <odango1329@gmail.com>
2025-02-10 15:33:08 +0900

    Implement main function to read prompt, process chat response, and write output to file

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
+from model.chat import chat
+from pathlib import Path
+from utils.utils import write_file, read_file
+from fire import Fire
+
+BABEL_PROMPT_PATH = Path(__file__).parent.parent / "data" / "prompt" / "babel.txt"
+OUTPUT_PATH = Path(__file__).parent.parent / "data" / "output" / "output.txt"
+
+
+def main():
+    prompt = read_file(BABEL_PROMPT_PATH)
+    response = chat(prompt)
+    write_file(OUTPUT_PATH, response)
+
 if __name__ == "__main__":
-    print("Hello World!")
+    Fire(main)
+    
